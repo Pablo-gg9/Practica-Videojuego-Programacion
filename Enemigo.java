c7fb7d831a4b1cdf3f2626d96ad23d06f2e8841e
Jarizapuldio <javierarizaxd@gmail.com>
2025-11-13 14:08:30 +0100

    Creacion clase JefeFinal y añadimos la funcion atacar

--- Enemigo.java
+++ Enemigo.java
@@ -6,88 +6,8 @@ package videojuegorpg;
 
 /**
  *
- * @author Alumno
+ * @author Javier Ariza
  */
-
-import java.lang.Math;
-
 public class Enemigo {
-    
-    private String nombre;
-    private int PS, armor, PA, vel, gold, nivel;
-
-    public Enemigo(String nombre, int nivel) {
-        this.nombre = nombre;
-        this.nivel = nivel;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getPS() {
-        return PS;
-    }
-
-    public void setPS(int PS) {
-        this.PS = PS;
-    }
-
-    public int getArmor() {
-        return armor;
-    }
-
-    public void setArmor(int armor) {
-        this.armor = armor;
-    }
-
-    public int getPA() {
-        return PA;
-    }
-
-    public void setPA(int PA) {
-        this.PA = PA;
-    }
-
-    public int getVel() {
-        return vel;
-    }
-
-    public void setVel(int vel) {
-        this.vel = vel;
-    }
-
-    public int getGold() {
-        return gold;
-    }
-
-    public void setGold(int gold) {
-        this.gold = gold;
-    }
-
-    @Override
-    public String toString() {
-        return "Enemigo{" + "nombre=" + nombre + ", PS=" + PS + ", armor=" + armor + ", PA=" + PA + ", vel=" + vel + ", gold=" + gold + '}';
-    }
-    
-    public void calcularEstadisticasEnemigo()
-    {
-        int PARandom = (int)(Math.random()*10) * nivel;
-        int PSRandom = (int)(Math.random()*11)+ 10 * nivel/2;
-        int velRandom = (int)(Math.random()*10) * nivel;
-        int armorRandom = (int)(Math.random()*4) + nivel;
-        int goldRandom = (int)(Math.random()*4) + nivel;
-        
-        PA = PARandom;
-        PS = PSRandom;
-        vel = velRandom;
-        armor = armorRandom;
-        gold = goldRandom;
-    }
-    
     
 }
